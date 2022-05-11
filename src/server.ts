6122fb899702a4d5bc963d4f42acb05245b74507
André Clinio <clinio@tecgraf.puc-rio.br>
2022-05-11 17:49:31 -0300

    handlers I

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import { Config } from './config';
-import { Logger } from './logger';
+import { Handler, IssueHandler, PushHandler } from './handler';
 
+enum Kind {
+  PUSH = "push",
+  TAG = "tag",
+  ISSUE = "issue",
+}
 export class Server {
 
   private readonly port;
   private readonly logger;
+  private readonly pushHandler;
+  private readonly issueHandler;
 
   constructor(config: Config) {
     this.port = config.getPort();
     this.logger = config.logger;
+    this.pushHandler = new PushHandler(this.logger);
+    this.issueHandler = new IssueHandler(this.logger);
   }
 
   run(): void {
@@ -19,13 +28,33 @@ export class Server {
     app.post('/', (req: Request, res: Response) => {
       const body = req.body;
       const bodyAsString = JSON.stringify(body);
-      const kind = body.object_kind;
-      this.logger.log(`WebHook activated body :${kind}`);
-      res.status(200);
+      const kindRaw = body.object_kind
+      const kind = kindRaw as Kind;
+      const handler = this.getHandler(kind);
+      if (handler) {
+        handler.handle(body);
+        res.status(200);
+      }
+      else {
+        res.status(403);
+      }
     });
 
     app.listen(this.port, () => {
       this.logger.log(`Server is running at localhost:${this.port}`);
     });
+  }
+
+  private getHandler(kind: Kind): Handler | undefined {
+    switch (kind) {
+      case Kind.PUSH:
+        return this.pushHandler;
+      case Kind.ISSUE:
+        return this.issueHandler;
+      case Kind.TAG:
+      default:
+        this.logger.log(`Unrecognized kind ${kind}`);
+        return undefined;
+    }
   }
 }
